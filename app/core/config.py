c56daf6686ec85274c1869e1afafac321159b866
Doddy <175628685+maxlevelnpc@users.noreply.github.com>
2026-06-18 08:15:23 +0700

    tambah config.py

---
+++ app/core/config.py
@@ -0,0 +1,8 @@
+# Nilai maksimal history yang akan ditampilakn di UI (10-20)
+MAX_HISTORY: int = 10
+
+# Nilai maksimal bookmark (100-1000)
+MAX_BOOKMARKS: int = 666
+
+# Nilai maksimal kata untuk dijelajahi (10-30)
+MAX_EXPLORE_WORDS: int = 10
